88a2f8d47ac99527ab1ca8fd0766de2cadff66c2
Sean O'Hara <sohara@sohara.com>
2014-05-20 11:19:17 -0400

    Switch to erb.deface file to see if it will work

--- app/overrides/layouts_admin_print_buttons_decorator.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/admin/orders/edit",
-                     :name => "print_buttons",
-                     :insert_after => "erb[loud]:contains('csrf_meta_tags')",
-                     :partial => "spree/admin/orders/print_buttons",
-                     :disabled => false)
